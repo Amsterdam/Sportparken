d92be0d45162222080e7e4d21395c38701c07b30
kelvinpaska <k.paska@amsterdam.nl>
2017-06-06 12:00:35 +0200

    removed monumenten from sportparken

--- web/.jenkins/import/import.sh
+++ web/.jenkins/import/import.sh
@@ -6,7 +6,7 @@ set -u
 DIR="$(dirname $0)"
 
 dc() {
-	docker-compose -p monumenten -f ${DIR}/docker-compose.yml $*
+	docker-compose -p sportparken -f ${DIR}/docker-compose.yml $*
 }
 
 trap 'dc kill ; dc rm -f' EXIT
@@ -20,8 +20,8 @@ dc build
 dc up -d database
 sleep 50
 
-dc exec -T database update-table.sh bag bag_pand public monumenten
-dc exec -T database update-table.sh bag bag_nummeraanduiding public monumenten
+dc exec -T database update-table.sh bag bag_pand public sportparken
+dc exec -T database update-table.sh bag bag_nummeraanduiding public sportparken
 
 dc run --rm importer
 dc run --rm db-backup
